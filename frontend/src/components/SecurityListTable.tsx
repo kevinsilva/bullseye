224c9c75b28d309e7615425c298bf8a17c1a7a1c
kevinsilva <kevinsilva.j@gmail.com>
2024-03-27 22:34:34 +0000

    [feat]: adds home animation

--- frontend/src/components/SecurityListTable.tsx
+++ frontend/src/components/SecurityListTable.tsx
@@ -1,4 +1,4 @@
-import { TableContainer, Table, TableHead, TableRow, TableCell, Typography, TableBody, Box } from "@mui/material"
+import { TableContainer, Table, TableHead, TableRow, TableCell, Typography, TableBody, Box, Fade } from "@mui/material"
 import { SecurityListTableTypes, SecurityTypes } from "../utils/types"
 
 export default function SecurityListTable({ data, handleClick }: SecurityListTableTypes) {
@@ -8,35 +8,40 @@ export default function SecurityListTable({ data, handleClick }: SecurityListTab
     if (trend >= .20) return '#73CFFF'
     else return '#19FF9D'
   }
+
   return (
     <Box>
-      <Typography variant="h2" sx={{ fontWeight: "Bold", color: '#FFF', margin: "100px 0 120px", maxWidth: "500px" }}>Aiming <Typography variant="h2" component={"span"} sx={{color: "#18FF9D", fontWeight: "Bold"}}>Bullseye</Typography> on Your Favorite Stocks.</Typography>
-        <TableContainer>
-          <Table aria-label="securities list table">
-            <TableHead>
-              <TableRow sx={{ fontWeight: "Bold", fontSize: "18px" }}>
-                <TableCell sx={{ fontWeight: "Bold", fontSize: "18px" }}>Symbol</TableCell>
-                <TableCell sx={{ fontWeight: "Bold", fontSize: "18px" }}>Name</TableCell>
-                <TableCell sx={{ fontWeight: "Bold", fontSize: "18px" }}>Sector</TableCell>
-                <TableCell sx={{ fontWeight: "Bold", fontSize: "18px" }}>Country</TableCell>
-                <TableCell sx={{ fontWeight: "Bold", fontSize: "18px" }}>Trend</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-
-              {data.map((security: SecurityTypes) => (
-                <TableRow key={security.id} onClick={() => handleClick(security.ticker)} sx={{ cursor: "pointer", '&:hover': { backgroundColor: '#39464C' } }}>
-                  <TableCell >{security.ticker}</TableCell>
-                  <TableCell>{security.name}</TableCell>
-                  <TableCell>{security.sector}</TableCell>
-                  <TableCell>{security.country}</TableCell>
-                  <TableCell sx={{ color: setBackgroundColor(security.trend) }}>{security.trend}</TableCell>
+      <Fade in={true} timeout={{ enter: 1000 }}>
+        <Typography variant="h2" sx={{ fontWeight: "Bold", color: '#FFF', margin: "100px 0 120px", maxWidth: "500px" }}>Aiming <Typography variant="h2" component={"span"} sx={{color: "#18FF9D", fontWeight: "Bold"}}>Bullseye</Typography> on Your Favorite Stocks.</Typography>
+      </Fade>
+      <Fade in={true} timeout={{ enter: 2000 }}>
+          <TableContainer>
+            <Table aria-label="securities list table">
+              <TableHead>
+                <TableRow sx={{ fontWeight: "Bold", fontSize: "18px" }}>
+                  <TableCell sx={{ fontWeight: "Bold", fontSize: "18px" }}>Symbol</TableCell>
+                  <TableCell sx={{ fontWeight: "Bold", fontSize: "18px" }}>Name</TableCell>
+                  <TableCell sx={{ fontWeight: "Bold", fontSize: "18px" }}>Sector</TableCell>
+                  <TableCell sx={{ fontWeight: "Bold", fontSize: "18px" }}>Country</TableCell>
+                  <TableCell sx={{ fontWeight: "Bold", fontSize: "18px" }}>Trend</TableCell>
                 </TableRow>
-              ))}
+              </TableHead>
+              <TableBody>
 
-            </TableBody>
-          </Table>
-        </TableContainer>
+                {data.map((security: SecurityTypes) => (
+                  <TableRow key={security.id} onClick={() => handleClick(security.ticker)} sx={{ cursor: "pointer", '&:hover': { backgroundColor: '#39464C' } }}>
+                    <TableCell >{security.ticker}</TableCell>
+                    <TableCell>{security.name}</TableCell>
+                    <TableCell>{security.sector}</TableCell>
+                    <TableCell>{security.country}</TableCell>
+                    <TableCell sx={{ color: setBackgroundColor(security.trend) }}>{security.trend}</TableCell>
+                  </TableRow>
+                ))}
+
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Fade>
     </Box>
   )
 }
