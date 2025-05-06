a2728bb578e08e90c45d05f3ccce52e761eebf8f
kevinsilva <kevinsilva.j@gmail.com>
2025-05-06 16:44:10 +0100

    [chore] updates app url

--- frontend/src/graphql/client.ts
+++ frontend/src/graphql/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: import.meta.env.VITE_APP_URL,
+  uri: `${import.meta.env.VITE_API_URL}/graphql`,
   cache: new InMemoryCache(),
 });
 
