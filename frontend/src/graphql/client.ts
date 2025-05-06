878a56b3f10c4c35d5c06f53a13d7ebb2908a9ed
kevinsilva <kevinsilva.j@gmail.com>
2025-05-06 16:01:31 +0100

    [chore] adds env variable to frontend

--- frontend/src/graphql/client.ts
+++ frontend/src/graphql/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
+  uri: import.meta.env.VITE_APP_URL || 'http://localhost:4000/',
   cache: new InMemoryCache(),
 });
 
