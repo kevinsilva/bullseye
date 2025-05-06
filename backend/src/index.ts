0ded2838834aca10d290666844207b6f63cb7a84
kevinsilva <kevinsilva.j@gmail.com>
2025-05-06 16:34:10 +0100

    [chore] adds cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,28 @@
+import 'dotenv/config';
+import express from 'express';
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from '@apollo/server/express4';
 import schemas from './schemas';
 import resolvers from './resolvers/';
+import cors from 'cors';
 import { SERVER_PORT } from './utils/config';
+
+const app = express();
+app.use(cors());
 
 const server = new ApolloServer({
   typeDefs: schemas,
   resolvers: resolvers,
 });
 
-startStandaloneServer(server, { listen: { port: SERVER_PORT } })
-  .then(({ url }) => {
-    console.log(`Server ready at: ${url}`);
-  })
-  .catch((error) => {
-    console.error(error);
+async function startServer() {
+  await server.start();
+
+  app.use('/graphql', express.json(), expressMiddleware(server));
+
+  app.listen(SERVER_PORT, () => {
+    console.log(`Server ready at port ${SERVER_PORT}`);
   });
+}
+
+startServer();
